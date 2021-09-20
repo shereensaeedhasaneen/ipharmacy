e1124a2a2aebe4b6a8b7d44793a9d6f404db7a19
asd <sheerysaeed123@gmail.com>
2021-09-20 16:51:10 +0200

    complete

--- js/radioSelected.js
+++ js/radioSelected.js
@@ -5,4 +5,35 @@ $(document).ready(function() {
         $("div.desc").slideUp(1000);
         $("#type" + test).slideDown(1000);
     });
+
+    /////////// Select Location /////////////
+    $('.selected-row').hide();
+    $('select#location').change(function(){
+        $('.selected-row').hide();
+        var selectedCountry = $('select#location').children("option:selected").val();
+        $("."+selectedCountry).slideDown(1000);
+    })
+
+    ///////////// specific medicin ///////////
+    $(".medication").hide();
+    $("input#transfer2").click(function() {
+        $(".medication").slideDown(1000);
+    });
+
+    $("input#transfer1").click(function() {
+        $(".medication").slideUp(1000);
+    });
+
+    ///////////// button display on click in checkbox ///////////////
+   
+    $("input#consent").click(function() {
+        if($("input#consent").is(':checked')){
+            $('button.transfer-btn').prop('disabled' ,false)
+        }
+
+        else{
+            $('button.transfer-btn').prop('disabled' ,true)
+        }
+    });
+
 });
